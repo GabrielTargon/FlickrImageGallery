a0517ce747aeb03514ea6ab9d88338df771ccd9e
Gabriel Targon <gabrieltargon@gmail.com>
2020-11-29 15:45:17 -0300

    Initial Commit

---
+++ FlickrImageGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrImageGallery
+//
+//  Created by Gabriel Targon on 29/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
